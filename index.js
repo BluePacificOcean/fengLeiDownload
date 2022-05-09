fc8466680b0e47bd53643a685efd3aaf37018569
= <=>
2022-05-09 08:04:49 +0800

    实现了在线下载aria功能

--- index.js
+++ index.js
@@ -2,9 +2,31 @@
 exports.__esModule = true;
 var electron_1 = require("electron");
 var child_process_1 = require("child_process");
+var fs_1 = require("fs");
+var https_1 = require("https");
+function downloadAria2() {
+    console.group('download start');
+    return new Promise(function (resolve, reject) {
+        (0, https_1.get)("https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0-win-32bit-build1.zip", {
+            port: 443,
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36 Edg/101.0.1210.39'
+            }
+        }, function (res) {
+            res.on("data", function (data) {
+                console.log(data);
+                console.groupEnd();
+                (0, fs_1.writeFile)('aria2.zip', data, function () { resolve(); });
+            });
+        }).on("error", function (e) {
+            // alert(e)
+            reject(e);
+        });
+    });
+}
 function startAria2() {
     return new Promise(function (resolve, reject) {
-        (0, child_process_1.exec)(".\\aria2\\aria2c --conf-path .\\aria2\\aria2.conf", function (error, out, err) {
+        (0, child_process_1.exec)(".\\aria2\\aria2c --conf-path .\\conf\\aria2.conf", function (error, out, err) {
             if (error) {
                 reject(error);
             }
@@ -22,9 +44,18 @@ function createWin() {
     }).loadFile("./src/index.html");
 }
 electron_1.app.whenReady()
+    .then(function () { return (0, fs_1.stat)('./aria2', function (err, stats) {
+    if (err || !stats.isDirectory()) {
+        return downloadAria2();
+    }
+    else {
+        return Promise.resolve();
+    }
+}); })
     .then(function () {
-    startAria2();
+    // startAria2()
     createWin();
 })["catch"](function (error) {
-    console.log(error);
+    // alert(error)
+    console.error(error);
 });
--- index.ts
+++ index.ts
@@ -1,10 +1,34 @@
 import { app, BrowserWindow } from 'electron'
-import { exec, execFile } from 'child_process'
+import { exec } from 'child_process'
+import { stat, writeFile } from 'fs'
+import { get } from 'https'
 
-function startAria2() {
+
+function downloadAria2(): Promise<void> {
+    console.group('download start')
     return new Promise((resolve, reject) => {
-        exec(`.\\aria2\\aria2c --conf-path .\\aria2\\aria2.conf`, (error, out, err)=>{
-            if(error){
+        get(`https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0-win-32bit-build1.zip`, {
+            port: 443,
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36 Edg/101.0.1210.39'
+            },
+        }, res => {
+            res.on("data", data => {
+                console.log(data);
+                console.groupEnd()
+                writeFile('aria2.zip', data, () => { resolve() })
+            })
+        }).on("error", e => {
+            // alert(e)
+            reject(e)
+        })
+    })
+}
+
+function startAria2(): Promise<string> {
+    return new Promise((resolve, reject) => {
+        exec(`.\\aria2\\aria2c --conf-path .\\conf\\aria2.conf`, (error, out, err) => {
+            if (error) {
                 reject(error)
             }
             else {
@@ -24,10 +48,19 @@ function createWin() {
 }
 
 app.whenReady()
-.then(()=> {
-    startAria2()
-    createWin()
-})
-.catch(error => {
-    console.log(error);
-})
+    .then(() => stat('./aria2', (err, stats) => {
+        if (err || !stats.isDirectory()) {
+            return downloadAria2()
+        }
+        else {
+            return Promise.resolve()
+        }
+    }))
+    .then(() => {
+        // startAria2()
+        createWin()
+    })
+    .catch(error => {
+        // alert(error)
+        console.error(error);
+    })
