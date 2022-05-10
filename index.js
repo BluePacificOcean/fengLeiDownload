28ebf1603dc15e5d417e80bb1e70fd7f322e81a2
= <=>
2022-05-10 22:09:25 +0800

    实现了关闭窗口时关闭aria功能

--- index.js
+++ index.js
@@ -2,54 +2,33 @@
 exports.__esModule = true;
 var electron_1 = require("electron");
 var child_process_1 = require("child_process");
-var promises_1 = require("fs/promises");
-var https_1 = require("https");
-function downloadAria2() {
-    console.group('download start');
-    return new Promise(function (resolve, reject) {
-        (0, https_1.get)("https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0-win-32bit-build1.zip", {
-            port: 443,
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36 Edg/101.0.1210.39'
-            }
-        }, function (res) {
-            res.on("data", function (data) {
-                console.log(data);
-                console.groupEnd();
-                (0, promises_1.writeFile)('aria2.zip', data).then(function () { resolve(); });
-            });
-        }).on("error", function (e) {
-            reject(e);
-        });
-    });
-}
+var aria2_process = undefined;
+var win = undefined;
 function startAria2() {
-    return new Promise(function (resolve, reject) {
-        (0, child_process_1.exec)(".\\aria2\\aria2c --conf-path .\\config\\aria2.conf", function (error, out, err) {
-            if (error) {
-                reject(error);
-            }
-            else {
-                resolve(out);
-            }
-        });
+    aria2_process = (0, child_process_1.spawn)(".\\aria2\\aria2c", ['--conf-path', '.\\config\\aria2.conf']);
+    aria2_process.stdout.on('data', function (data) {
+        console.log(data.toString());
+    });
+    aria2_process.stderr.on('data', function (data) {
+        console.log(data.toString());
     });
 }
 function createWin() {
-    console.log("create");
     return new electron_1.BrowserWindow({
         // frame: false
         width: 1000,
         height: 750
-    }).loadFile("./src/index.html");
+    });
 }
 electron_1.app.whenReady()
-    // .then(() => stat('./aria2'))
-    // .then(stats => stats.isDirectory() ? Promise.resolve() : downloadAria2()
     .then(function () {
     startAria2();
-    createWin();
+    win = createWin();
+    win.loadFile("./src/index.html");
 })["catch"](function (error) {
-    // alert(error)
     console.error(error);
+});
+electron_1.app.on("window-all-closed", function () {
+    aria2_process.kill();
+    electron_1.app.quit();
 });
--- index.ts
+++ index.ts
@@ -1,60 +1,36 @@
 import { app, BrowserWindow } from 'electron'
-import { exec } from 'child_process'
-import { stat, writeFile } from 'fs/promises'
-import { get } from 'https'
+import { spawn, ChildProcessWithoutNullStreams } from 'child_process'
 
-
-function downloadAria2(): Promise<void> {
-    console.group('download start')
-    return new Promise((resolve, reject) => {
-        get(`https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0-win-32bit-build1.zip`, {
-            port: 443,
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36 Edg/101.0.1210.39'
-            },
-        }, res => {
-            res.on("data", data => {
-                console.log(data);
-                console.groupEnd()
-                writeFile('aria2.zip', data).then(() => { resolve() })
-            })
-        }).on("error", e => {
-            reject(e)
-        })
+let aria2_process: ChildProcessWithoutNullStreams = undefined
+let win: BrowserWindow = undefined
+function startAria2() {
+    aria2_process = spawn(`.\\aria2\\aria2c`, ['--conf-path', '.\\config\\aria2.conf'])
+    aria2_process.stdout.on('data', (data:Buffer) => {
+        console.log(data.toString())
+    })
+    aria2_process.stderr.on('data', (data:Buffer) => {
+        console.log(data.toString())
     })
 }
 
-function startAria2(): Promise<string> {
-    return new Promise((resolve, reject) => {
-        exec(`.\\aria2\\aria2c --conf-path .\\config\\aria2.conf`, (error, out, err) => {
-            if (error) {
-                reject(error)
-            }
-            else {
-                resolve(out)
-            }
-        })
-    })
-}
-
-
-function createWin() {
-    console.log("create");
+function createWin(): BrowserWindow {
     return new BrowserWindow({
-        // frame: false
         width: 1000,
         height: 750
-    }).loadFile("./src/index.html")
+    })
 }
 
 app.whenReady()
-    // .then(() => stat('./aria2'))
-    // .then(stats => stats.isDirectory() ? Promise.resolve() : downloadAria2()
     .then(() => {
         startAria2()
-        createWin()
+        win = createWin()
+        win.loadFile("./src/index.html")
     })
     .catch(error => {
-        // alert(error)
         console.error(error);
     })
+
+app.on("window-all-closed", ()=>{
+    aria2_process.kill()
+    app.quit()
+})
